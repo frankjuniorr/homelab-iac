a2b71992a4da17f3e509bfeda22f9b7ed6133853
Frank Junior <frankcbjunior@gmail.com>
2025-05-17 02:28:01 -0300

    update Rocky Linux installation

--- src/callback_plugins/beautiful_output.py
+++ src/callback_plugins/beautiful_output.py
@@ -878,6 +878,18 @@ class CallbackModule(CallbackBase, FileSystemEventHandler):
                 the argument ``verbosity``, False otherwise.
         """
         result = {} if not result else result._result
+
+        # FRANK JUNIOR: FORÇA mostrar sempre stderr e exception
+        #######################################################
+        is_error = (
+            result.get("failed", False)
+            or result.get("unreachable", False)
+            or result.get("rc", 0) != 0
+        )
+        if is_error and ("exception" in result or "stderr" in result or "module_stderr" in result):
+            return True
+        #######################################################
+
         return (
             self._display.verbosity >= verbosity or "_ansible_verbose_always" in result
         ) and "_ansible_verbose_override" not in result
@@ -1127,7 +1139,7 @@ class CallbackModule(CallbackBase, FileSystemEventHandler):
             else:
                 item_name = u'JSON: "{0}"'.format(
                     stringtruncate(
-                        json.dumps(item_name, separators=(",", ":")), width=36
+                        json.dumps(item_name, separators=(",", ":")), width=0
                     )
                 )
         task_host = self._get_host_string(result, "@")
